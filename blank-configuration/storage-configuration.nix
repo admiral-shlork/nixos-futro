789448568d19c7303f376d8d826272face7b096e
admiral-shlork <vvhtvr@protonmail.ch>
2025-11-27 11:58:40 +0000

    fixing storage config

--- blank-configuration/storage-configuration.nix
+++ blank-configuration/storage-configuration.nix
@@ -5,30 +5,28 @@
     initrd.availableKernelModules = [ "ahci" "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
     initrd.kernelModules = [ "dm-snapshot" ];
     kernelModules = [ "kvm-intel" ];
-    kernelPackages = [ ]; #pkgs.linuxPackages_6_12; #kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = [ ];
     kernelParams = [ ];
     extraModulePackages = [ ];
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+  };
 
-  # swapDevices = [{ 
-      # device = "/dev/disk/by-uuid/c30a3550-ab3b-4820-afc7-b833f4f3b36c";
-      # device = "/swapfile";
-      # size = 64 * 1024;
+  # swapDevices = [{
+  #     device = "/swapfile";
+  #     size = 64 * 1024;
   #   }];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/root";
-      fsType = "ext4";
-    };
 
   fileSystems."/boot" =
     { device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
+      options = [ "fmask=0022" "dmask=0022" ];
     };
-  };
+  
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/root";
+      fsType = "ext4";
+    };
 }
-
