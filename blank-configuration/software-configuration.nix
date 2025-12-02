beb53d7d14095a3f3767435d9ae9bc93b0077847
admiral-shlork <vvhtvr@protonmail.ch>
2025-12-02 13:40:51 +0000

    enabled AdGuard Home service

--- blank-configuration/software-configuration.nix
+++ blank-configuration/software-configuration.nix
@@ -2,6 +2,11 @@
 {
   services.xserver.enable = false;
   services.openssh.enable = true;
+  services.adguardhome = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 3000;
+  }
 
   environment.systemPackages = with pkgs; [
     adguardhome
